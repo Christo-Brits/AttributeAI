488d7c29524dafce946f7c7789f06545486a520f
Christo Brits <infinitebuildsolutions2024@gmail.com>
2025-07-06 13:23:48 +1200

    ≡ƒöº Fix deployment - Use real App instead of test app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';  // Commented out for testing
-import AppTestSocial from './AppTestSocial';  // Testing social auth
+import App from './App';  // Real AttributeAI app
+// import AppTestSocial from './AppTestSocial';  // Testing social auth - DISABLED
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppTestSocial />
+    <App />
   </React.StrictMode>
 );
